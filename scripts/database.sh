71628f097fbc45f5353f6ce77166b8792df32893
brad-anton <brad.antonie@gmail.com>
2012-11-19 20:43:39 -0500

    fixed database.sh

--- scripts/database.sh
+++ scripts/database.sh
@@ -2,10 +2,11 @@
 # Created by Josh Wright
 # updated by brad antoniewicz
 
+SQLITE_BIN=sqlite3
 
 echo "[+] Creating bnapbnap.db"
 echo -e "[+] \tCreating bnap table"
-sqlite bnapbnap.db "
+$SQLITE_BIN bnapbnap.db "
 	CREATE TABLE bnap (
 		bdaddr0		TEXT NOT NULL,
 		bdaddr1		TEXT NOT NULL,
@@ -19,7 +20,7 @@ sqlite bnapbnap.db "
 		PRIMARY KEY (bdaddr0, bdaddr1, bdaddr2, bdaddr3, submitterhost)
 	)"
 echo -e "[+] \tCreating oui table"
-sqlite bnapbnap.db "
+$SQLITE_BIN bnapbnap.db "
 	CREATE TABLE oui (
 		oui		TEXT NOT NULL,
 		vendor		TEXT NOT NULL,
@@ -30,11 +31,11 @@ echo "[+] Sanitizing oui.txt"
 iconv -f windows-1252 -t utf-8 oui.txt > oui.iconv
 
 echo "[+] Importing oui.iconv into bnapbnap.db"
-./oui-insert.rb oui.iconv | sqlite bnapbnap.db
+./oui-insert.rb oui.iconv | $SQLITE_BIN bnapbnap.db
 
 echo "[+] Importing jlw-entries.sql into bnapbnap.db"
-cat jlw-entries.sql | sqlite bnapbnap.db 
-#sort -u jlw-entries.sql | sqlite bnapbnap.db 
+#cat jlw-entries.sql | $SQLITE_BIN bnapbnap.db 
+sort -u jlw-entries.sql | grep -v "127.0.0.2" | $SQLITE_BIN bnapbnap.db 
 
 echo "[+] Cleaning up"
 rm oui.iconv
